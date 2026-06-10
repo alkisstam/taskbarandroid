521a7ba2de29d8f1a22d29252cc3c30cf6adb075
alkisstam <alkiss87@gmail.com>
2026-06-10 13:32:06 +0200

    Bump to v1.2.7 (30)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.alkisstam.taskbar"
         minSdk = 26
         targetSdk = 35
-        versionCode = 28
-        versionName = "1.2.6"
+        versionCode = 30
+        versionName = "1.2.7"
         ndk {
             debugSymbolLevel = "FULL"
         }
